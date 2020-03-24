4b34df436705573b7db4f17bbe5739a9caf48407
devilroop <61988264+devilroop@users.noreply.github.com>
2020-03-24 16:23:54 +0530

    Add files via upload

---
+++ hm 23.py
@@ -0,0 +1,12 @@
+#concatenate two strings and length of concatenated strings and #pnja
+a="python"
+b="java"
+c=a+b
+print(c)
+print(len(c))
+print(a[0]+a[-1]+b[0]+b[-1])
+#print PythoN
+a="python"
+b="PYTHON"
+c=b[0]+a[1:5]+b[-1]
+print(c)
